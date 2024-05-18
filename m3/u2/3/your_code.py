7eda55d882cda7cd4bb08afc71ccd72893dc124b
Makson <gorvatmaksim5@gmail.com>
2024-05-18 09:04:49 +0300

    Дз 5 з 8

--- m3/u2/3/your_code.py
+++ m3/u2/3/your_code.py
@@ -2,11 +2,72 @@ import pandas as pd
 
 df = pd.read_csv('m3/u1/1/GoogleApps.csv')
 
-makson = df.pivot_table(
-    columns='Type',
-    index='Content Rating',
-    values='Size',
-    aggfunc=['min','mean']
-)
+#makson = df.pivot_table(
+ #   columns='Type',
+ #   index='Content Rating',
+ #   values='Size',
+  #  aggfunc=['min','mean']
+#)
 
-print(makson)
+#print(makson)
+
+
+
+#1)
+'''
+fb = (df['Category'].value_counts())
+fbb = fb['BUSINESS']
+print(fbb)'''
+
+
+
+#2)
+'''
+fb = (df['Content Rating'].value_counts())
+fbb = fb['Teen']
+bb = fb['Everyone 10+']
+
+gh = fbb/bb
+
+print(gh)
+'''
+
+
+
+#3)
+#1
+'''
+b = (df[df['Type'] == 'Paid']['Rating'].mean())
+print(b)
+'''
+
+
+
+#2
+'''
+a = (df[df['Type'] == 'Free']['Rating'].mean())
+b = (df[df['Type'] == 'Paid']['Rating'].mean())
+
+c = b-a
+print(c)
+'''
+
+
+#4)
+'''
+contents = df.groupby(by = [df['Rating' ]== "COMICS"])['Size'].agg(['min','max'])
+print(contents)
+'''
+#5)
+#1
+#fb = (df[df['Rating']== 4.5]['Category'].value_counts())
+#fbb = fb["FINANCE"]
+#print(fbb)
+
+
+#2
+mb = (df[df['Rating']== 4.9]['Type'].value_counts())
+fre = mb["Free"]
+plat = mb["Paid"]
+rez = fre/plat
+print(rez)
