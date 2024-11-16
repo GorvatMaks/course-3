f355fc13d64ab72aacd0b46ede238b0edd14b695
Makson <gorvatmaksim5@gmail.com>
2024-11-16 19:26:21 +0200

    16.11(templates)

---
+++ m6/u5/db.py
@@ -0,0 +1,159 @@
+import sqlite3
+from pprint import pprint
+conn = None
+
+curs = None
+
+def createTable():
+    open()
+    do('PRAGMA foreign_keys=on')
+
+    do('''
+        CREATE TABLE IF NOT EXISTS quiz(
+            id INTEGER PRIMARY KEY,
+            name VARCHAR)                        
+    ''')
+
+
+    do('''
+        CREATE TABLE IF NOT EXISTS question(
+            id INTEGER PRIMARY KEY,
+            question VARCHAR, 
+            answer VARCHAR,
+            wrong1 VARCHAR,
+            wrong2 VARCHAR,
+            wrong3 VARCHAR
+        )                              
+    ''')
+
+
+    do('''
+        CREATE TABLE IF NOT EXISTS  quiz_content(
+            id INTEGER PRIMARY KEY,
+            quiz_id INTEGER,
+            question_id INTEGER,
+            FOREIGN KEY (question_id) REFERENCES question (id),
+            FOREIGN KEY (quiz_id) REFERENCES quiz (id)
+        )                        
+    ''')
+    
+    close()
+
+
+def do(query):
+    curs.execute(query)
+    conn.commit()
+
+
+def close():
+    curs.close()
+    conn.close()
+
+
+def open():
+    global conn, curs
+    conn = sqlite3.connect("m6/u5/Quiz.db")
+
+    curs = conn.cursor()
+
+
+def clear_db():
+    open()
+    do('DROP TABLE IF EXISTS quiz_content')
+    do('DROP TABLE IF EXISTS question')
+    do('DROP TABLE IF EXISTS quiz')
+    close()
+
+
+def add_question():
+    questions = [
+        ('Хто мій вчитель з програмування?', 'Володмир','бублик','ластівка','я'),
+        ('Скільки місяців на рік мають 28 днів?', 'Всі', 'Один', 'Жодного', 'Два'),
+        ('Яким стане зелена скеля, якщо впаде в Червоне море?', 'Мокрим', 'Червоним', 'Не зміниться', 'Фіолетовим'),
+        ('Якою рукою краще розмішувати чай?', 'Ложкою', 'Правою', 'Лівою', 'Любою'),
+        ('Що не має довжини, глибини, ширини, висоти, а можна виміряти?', 'Час', 'Дурність', 'Море', 'Повітря')
+    ]
+    open()
+    curs.executemany(''' 
+        INSERT INTO question (question, answer, wrong1, wrong2, wrong3)
+        VALUES (?,?,?,?,?)
+        ''', questions)
+    conn.commit()
+    close()
+
+def add_quiz():
+    quizs = [
+        ('Початковий квіз',),
+        ('Середній квіз',),
+        ('Хард квіз',)
+    ]
+    open()
+    curs.executemany('''
+        INSERT INTO quiz (name)
+        VALUES (?)
+        ''', quizs)
+    conn.commit()
+    close()
+
+
+def add_question_on_quiz():
+    open()
+    do('PRAGMA foreign_keys=on')
+
+    query = '''
+        INSERT INTO quiz_content (quiz_id, question_id)
+        VALUES (?,?)
+        '''
+    input1 = input("Чи додати зв'зок?(Y/N)")
+    
+    while input1 != 'N':
+        quiz_input = int(input("Id квіза"))
+        question_input = int(input("Id запитання"))
+        curs.execute(query,[quiz_input, question_input])
+        conn.commit()
+        input1 = input("Чи додати зв'зок?(Y/N)")
+    close()
+
+def show(Table):
+    open()
+    curs.execute("SELECT * FROM " + Table)
+    data = curs.fetchall()
+    pprint(data)
+    close()
+
+def showTables():
+    show("question")    
+    show("quiz")    
+    show("quiz_content")
+
+def getQuestion(question_id, quiz_id):
+    open()
+    query = ''' 
+    SELECT quiz_content.id, question.question, question.answer, question.wrong1, question.wrong2, question.wrong3
+    FROM question, quiz_content
+    WHERE quiz_content.question_id == question.id
+    AND quiz_content.id >= ? 
+    AND quiz_content.quiz_id == ?
+    ORDER BY quiz_content.id
+    '''
+    curs.execute(query,[question_id, quiz_id])
+    rezalt = curs.fetchone()
+    close()
+    return rezalt
+
+def getQuizs():
+    open()
+    query = 'SELECT * FROM quiz ORDER BY name'
+    curs.execute(query)
+    rezalt = curs.fetchall()
+    close()
+    return rezalt
+
+if __name__ == "__main__":
+    #clear_db()
+    #createTable()
+    #add_question()
+    #add_quiz()
+    showTables()
+    #add_question_on_quiz()
+    print(getQuestion(1,1))
--- m6/u5/main.py
+++ m6/u5/main.py
@@ -1,21 +1,109 @@
-from flask import Flask,redirect,url_for,session
+from flask import Flask,redirect,url_for,session, request
+from random import randint
+from db import getQuestion, getQuizs
+import os 
+
+folder = os.getcwd()
+folder_templates = os.path.join(folder,"m6\\u5\\templates")
+print("GGGGGG",folder)
+print(folder_templates)
+
+
+
+def startQuiz(quiz_id):
+    session['quiz'] = quiz_id
+    session['last_question'] = 0
+
+def quizForm():
+    htmlbequin = '''
+        <html>
+            <body>
+                <h1>Виберіть вікторину</h1>
+                <form method="post" action="/">
+                <select name="quiz">
+    '''
+
+    formSubmit = '''
+        <button type="submit">Надіслати</button>
+    '''
+
+    htmlend = f'''
+                </select>
+                {formSubmit}
+                </form>
+            </body>
+        </html>  
+    '''
+
+    quizAll = getQuizs()
+   
+    opts = ""
+    for id, name in quizAll:
+        opt = f'''
+            <option value={id}>
+                {name}
+            </option>
+        '''
+        opts += opt
+    return htmlbequin + opts + htmlend
 
 
 def index():
-    return "Папа"
-
+    if request.method == "GET":
+        startQuiz(-1)
+        return quizForm()
+    else:
+        timequiz = request.form.get("quiz")
+        startQuiz(timequiz)
+        return redirect(url_for("test"))
 
 def test():
-    return redirect(url_for("result"))
+    if int(session.get("quiz", 0)) <= 0:
+        return redirect(url_for("index"))
+
+    Question = getQuestion(session['last_question'], session['quiz'])
+    
+    print(Question)
+    
+    if Question == None or len(Question) == 0:
+        return redirect(url_for("result"))
+    
+    html = f"""
+    <h1>{Question[1]}</h1>
+    <fieldset>
+        <legend>Select a maintenance drone:</legend>
+
+    <div>
+        <input type="radio" id="huey" name="drone" value="huey" checked />
+        <label for="huey">Huey</label>
+    </div>
+
+    <div>
+        <input type="radio" id="dewey" name="drone" value="dewey" />
+        <label for="dewey">Dewey</label>
+    </div>
+
+    <div>
+        <input type="radio" id="louie" name="drone" value="louie" />
+        <label for="louie">Louie</label>
+    </div>
+    </fieldset>
+
+    """
+    
+    
+    return html
 
 
 def result():
     return "бу"
 
 
-Saut = Flask(__name__)
+Saut = Flask(__name__, template_folder=folder_templates, static_folder=folder_templates)
 
-Saut.add_url_rule('/', 'index', index)
+Saut.config["SECRET_KEY"] = "GGG"
+
+Saut.add_url_rule('/', 'index', index, methods=['POST', 'GET'])
 Saut.add_url_rule('/test', 'test', test)
 Saut.add_url_rule('/result', 'result', result)
 
@@ -23,5 +111,4 @@ Saut.add_url_rule('/result', 'result', result)
 
 
 
-
-Saut.run()
+Saut.run(debug=True)
