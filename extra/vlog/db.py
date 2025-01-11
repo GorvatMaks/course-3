812a8c674a442e25bf4bcb732b78616bbfc70a91
Makson <gorvatmaksim5@gmail.com>
2025-01-11 13:25:12 +0200

    vlog lesson 2

--- extra/vlog/db.py
+++ extra/vlog/db.py
@@ -1,1 +1,23 @@
+import settings
+import sqlite3
+from pprint import pprint
 
+
+conn = None
+curs = None
+
+def do(query):
+    curs.execute(query)
+    conn.commit()
+
+
+def close():
+    curs.close()
+    conn.close()
+
+
+def open():
+    global conn, curs
+    conn = sqlite3.connect(settings.DB_URL)
+    conn.row_factory = sqlite3.Row
+    curs = conn.cursor()
--- extra/vlog/main.py
+++ extra/vlog/main.py
@@ -1,14 +1,28 @@
 from flask import Flask,redirect,url_for,session, request, render_template
+import settings
 
-app = Flask(__name__)
-
-
-
+app = Flask(__name__, template_folder=settings.TEMPLATES_URL, static_folder=settings.STATIC_URL)
 
 
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
 
 
+@app.route("/post/category")
+def post_category():
+    return "c"
 
+
+@app.route("/post/view")
+def post_view():
+    return "v"
+
+
+@app.route("/about")
+def about():
+    return "a"
 
 
 app.config["SECRET_KEY"] = "GGG"
